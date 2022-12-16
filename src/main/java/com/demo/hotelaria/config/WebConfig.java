44be147e1618f0224ebc845f4c91a16d7e5a4e24
kailaniestevesbarboza <kailaniesteves@gmail.com>
2022-12-16 20:10:05 -0300

    Updating CORS

--- src/main/java/com/demo/hotelaria/config/WebConfig.java
+++ src/main/java/com/demo/hotelaria/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.demo.hotelaria.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,8 +10,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/hotelaria/demo/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE").allowedHeaders("*");
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE").allowedHeaders("*");
+//    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE").allowedHeaders("*");
+            }
+        };
     }
 }
